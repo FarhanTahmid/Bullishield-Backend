a51dee3d924bcb437e1f25c9e3e8f792a32c0e77
Farhan Tahmid <farhantahmid881@gmail.com>
2024-06-16 02:09:39 +0600

    created complete api for signup

--- bullishield_backend/urls.py
+++ bullishield_backend/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('system.urls',namespace='system')),
+    path('user/',include('user.urls',namespace='user'))
     
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth.models import User,auth
+from django.contrib.auth.hashers import make_password
+from user.models import *
+
+
 
 # Create your views here.
+class UserSignup(APIView):
+    
+    def post(self,request):
+        # get json data
+        data = request.data
+        # get username password and email from json data
+        username = data.get('username')
+        email = data.get('email')
+        
+        password = data.get('password')
+
+        # Check if the username and email are already taken
+        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+            return Response({'error': 'User with username or email already exists'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        # Create a new user if only a member is registered in the organization database
+        try:
+            UserInformations.objects.get(user_id=username)
+            new_user = User.objects.create(
+                username=username,
+                email=email,
+                password=make_password(password)
+            )
+            new_user.save()
+            return Response({'success': f'Signup successful. A new user with {username} was created'}, status=status.HTTP_201_CREATED)
+
+        except UserInformations.DoesNotExist:
+            return Response({'error': 'Username Does not exist in the Organisation Database!'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        except Exception as e:
+            return Response({'error': 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
+            
